8421848df906a8036be5dd12d22b9051e0d15c7c
UlaşDK <udk97@hotmail.com>
2023-02-18 14:09:30 +0300

    first fetch

--- weatherapp/src/redux/weatherSlice.js
+++ weatherapp/src/redux/weatherSlice.js
@@ -1,10 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const weatherSlice = createSlice({
-    name : "weather",
-    initialState,
-    reducers:{}
+export const fetchData = createAsyncThunk("weather/getWeaterDate",async()=>{
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/weather?q=Ankara&appid=${process.env.REACT_APP_API_KEY}`)
+    return res.data
 })
+
+
+export const weatherSlice = createSlice({
+  name: "weather",
+  initialState:{
+    data : [],
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchData.fulfilled]: (state,action)=>{
+        console.log(action.payload)
+    }
+  }
+});
 
 export default weatherSlice.reducer
