85f900e6421746b5e8dfc35deec63dbdfd681841
UlaşDK <udk97@hotmail.com>
2023-02-22 18:27:20 +0300

    dropdown

--- weatherapp/src/components/Cities.js
+++ weatherapp/src/components/Cities.js
@@ -32,7 +32,7 @@ export default function Cities() {
         <IoIosArrowDropdownCircle className="ml-2"/>
       </button>
       <div id="dropdown"
-        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+        className="z-10  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
         <ul
           className="py-2 text-sm text-gray-700 dark:text-gray-200"
           aria-labelledby="dropdownDefaultButton">
